3459c6a7cad8374e07b92fc61d1a8393a7237fa1
Jeffrey <a31185367@gmail.com>
2025-03-18 08:29:40 +0800

    新增計算bmi功能
    尚未更新name, gender讀取

--- Project Implementation I/app/src/main/java/com/example/projectimplementationi/MainActivity.kt
+++ Project Implementation I/app/src/main/java/com/example/projectimplementationi/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.example.projectimplementationi
 
-import android.os.Build
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioGroup
+import android.widget.Switch
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import java.time.LocalDate
+import kotlin.math.pow
 
 
 class MainActivity : AppCompatActivity() {
@@ -28,25 +27,37 @@ class MainActivity : AppCompatActivity() {
         val edName = findViewById<EditText>(R.id.edName)
         val edHeight = findViewById<EditText>(R.id.edHeight)
         val edWeight = findViewById<EditText>(R.id.edWeight)
-        val rdgrpGender = findViewById<RadioGroup>(R.id.rdgrpGender)
+        val rdgrpGender = findViewById<RadioGroup>(R.id.rdgrpGender) // 性別Radio Group
         val btnCalculate = findViewById<Button>(R.id.btnCalculate)
-        val tvWarning = findViewById<TextView>(R.id.tvWarning)
+        val tvText = findViewById<TextView>(R.id.tvText)
         btnCalculate.setOnClickListener{
+            // 確認所有資訊已被填寫
             if(edName.text.isEmpty()||edHeight.text.isEmpty()||edWeight.text.isEmpty()){
-                tvWarning.text ="填入完整訊息"
+                tvText.text ="填入完整訊息"
                 return@setOnClickListener
             }
+            // 限縮資料範圍，防呆
             if(edHeight.text.toString().toDouble()<0 || edHeight.text.toString().toDouble()>=3){
-                tvWarning.text ="填入正確的身高(公尺)"
+                tvText.text ="填入正確的身高(公尺)"
                 return@setOnClickListener
             }
             if(edWeight.text.toString().toDouble()<0 || edWeight.text.toString().toDouble()>300){
-                tvWarning.text ="填入正確的體重(公斤)"
+                tvText.text ="填入正確的體重(公斤)"
                 return@setOnClickListener
             }
+            // 讀取資料、計算BMI
             val name = edName.text.toString()
-            val weight = edWeight.text.toString()
+            val weight = edWeight.text.toString().toDouble()
+            val height = edWeight.text.toString().toDouble() // 次方要用double
+            val bmi = weight/(height.pow(2))
+            tvText.text = when{
+                bmi < 18.5 -> "過輕"
+                bmi >= 18.5 && bmi < 24.0 -> "正常"
+                bmi >= 24.0 && bmi < 27.0 -> "過重"
+                bmi >= 27.0 && bmi < 30.0 -> "輕度肥胖"
+                bmi >= 30 && bmi < 35.0 -> "中度肥胖"
+                else -> "重度肥胖"
+            }
         }
-
     }
 }
